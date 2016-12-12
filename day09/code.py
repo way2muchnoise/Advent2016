0ca03a23fb64471645a6845168ded385141067e1
way2muchnoise <jens.baetens@live.be>
2016-12-12 20:25:08 +0100

    check in day02 part02

--- day09/code.py
+++ day09/code.py
@@ -1,3 +1,5 @@
+import re
+
 compressed = open('input.txt')
 uncompressed = ''
 c = compressed.read(1)
@@ -20,3 +22,22 @@ while c:
     c = compressed.read(1)
 print uncompressed
 print len(uncompressed)
+compressed.close()
+
+pattern = re.compile(r'\((\d+)x(\d+)\)')
+
+
+def recurse_decompress(string, pos=0, endpos=None):
+    match = pattern.search(string[pos:endpos])
+    length = 0
+    while match:
+        times = int(match.group(2))
+        length += match.start(0)
+        length += recurse_decompress(string, pos + match.end(0), pos + match.end(0) + int(match.group(1))) * times
+        pos = pos + match.end(0) + int(match.group(1))
+        match = pattern.search(string[pos:endpos])
+    if length is 0:
+        length = endpos - pos
+    return length
+
+print recurse_decompress(open('input.txt').readline().strip())
