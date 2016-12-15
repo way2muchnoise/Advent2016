a1beba521c85493ee51629b5d86d0f89a1d0f62e
way2muchnoise <jens.baetens@live.be>
2016-12-15 19:03:28 +0100

    check in day10
    fully working now

--- day10/code.py
+++ day10/code.py
@@ -5,6 +5,7 @@ input_pattern = re.compile('(\d+) .* (\d+)')
 pass_pattern = re.compile('(\d+) .* (.*?) (\d+) .* (.*?) (\d+)')
 bots = {}
 passes = {}
+outputs = {}
 for line in lines:
     if line.startswith('value'):
         match = input_pattern.search(line)
@@ -15,38 +16,38 @@ for line in lines:
     else:
         match = pass_pattern.search(line)
         passes[match.group(1)] = (
-            match.group(3) if match.group(2) == 'bot' else None,
-            match.group(5) if match.group(4) == 'bot' else None
+            match.group(3) if match.group(2) == 'bot' else '-' + match.group(3),
+            match.group(5) if match.group(4) == 'bot' else '-' + match.group(5)
         )
 print bots
 print passes
 
 
 def sort(bot, result):
-    vals = sorted(bots[bot])
-    if len(result) == 0 and vals[0] == 61 and vals[1] == 17:
+    vals = sorted(bots[bot], key=lambda x: int(x))
+    bots[bot] = []
+    if vals[1] == '61' and vals[0] == '17':
         result.append(bot)
-        return
     hands = passes[bot]
-    # High
-    if hands[1] is None:
-        pass
-    elif hands[1] in bots:
-        bots[hands[1]].append(vals[0])
-    else:
-        bots[hands[1]] = [vals[0]]
     # Low
-    if hands[0] is None:
-        pass
+    if hands[0].startswith('-'):
+        outputs[hands[0][1:]] = vals[0]
     elif hands[0] in bots:
-        bots[hands[0]].append(vals[1])
+        bots[hands[0]].append(vals[0])
     else:
-        bots[hands[0]] = [vals[1]]
+        bots[hands[0]] = [vals[0]]
+    # High
+    if hands[1].startswith('-'):
+        outputs[hands[1][1:]] = vals[1]
+    elif hands[1] in bots:
+        bots[hands[1]].append(vals[1])
+    else:
+        bots[hands[1]] = [vals[1]]
     # Recursive sort
-    if hands[1] and len(bots[hands[1]]) > 1:
-        sort(hands[1], result)
-    if hands[0] and len(bots[hands[0]]) > 1:
+    if not hands[0].startswith('-') and len(bots[hands[0]]) > 1:
         sort(hands[0], result)
+    if not hands[1].startswith('-') and len(bots[hands[1]]) > 1:
+        sort(hands[1], result)
 
 result = []
 start_bot = None
@@ -54,6 +55,7 @@ for num, vals in bots.iteritems():
     if len(vals) > 1:
         start_bot = num
         break
-print start_bot
 sort(start_bot, result)
+print bots
 print result
+print int(outputs['0']) * int(outputs['1']) * int(outputs['2'])
